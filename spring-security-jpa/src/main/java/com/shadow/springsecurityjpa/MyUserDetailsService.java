f523c5b77411687d855ecdabaf02f54077a51bf2
exo <exo@192.168.0.15>
2020-10-05 00:52:15 -0400

    First Commit
    Saving JPA Login System

---
+++ spring-security-jpa/src/main/java/com/shadow/springsecurityjpa/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.shadow.springsecurityjpa;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.shadow.springsecurityjpa.models.MyUserDetails;
+import com.shadow.springsecurityjpa.models.User;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> user = userRepository.findByUserName(username);
+		user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+		
+		return user.map(MyUserDetails::new).get();
+	}
+	
+
+
+}
